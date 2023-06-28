4a3db070ea1960831ec48475a9eb62b7c3073e94
lvroc <lvroc.gnz@outlook.com>
2023-06-27 19:30:44 -0600

    update

--- MemoryGame/js/gameManajer.js
+++ MemoryGame/js/gameManajer.js
@@ -2,8 +2,8 @@ import { MenuController } from "./controllers/menu/menuController.js";
 
 export class GameManager{
     constructor(){
-        this.navigationContainer = document.getElementById('navigationContainer');
-        this.contentContainer = document.getElementById('contentContainer')
-        this.controller = new MenuController(this,this.contentContainer);
+        let navigationContainer = document.getElementById('navigationContainer');
+        let contentContainer = document.getElementById('contentContainer')
+        this.controller = new MenuController(this,contentContainer);
     }
 }
