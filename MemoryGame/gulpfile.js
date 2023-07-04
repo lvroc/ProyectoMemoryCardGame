476287de4ff102f509060f2cc1319b4dcd564603
lvroc <lvroc.gnz@outlook.com>
2023-07-03 21:09:11 -0600

    updates

---
+++ MemoryGame/gulpfile.js
@@ -0,0 +1,29 @@
+const gulp =require('gulp');
+const rollup = require('rollup');
+const clean = require('gulp-clean');
+
+const browsersync = require('browser-sync');
+const server = browsersync.create();
+
+gulp.task('clean', ()=>{
+    return gulp.src('node_modules').pipe(clean());
+});
+
+exports.bundle = () => {
+    console.log('GULP - Running bundle function');
+}
+
+exports.play = () =>  {
+    console.log('running game...');
+    server.init({
+        server:{
+            baseDir: '.'
+        }
+    });
+};
+
+
+
+/*
+    comandos npm run clean , esta en scripts
+*/
