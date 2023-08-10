b90508e972c54a91d4e1c7229105dc21140061ba
lvroc <lvroc.gnz@outlook.com>
2023-08-09 18:45:32 -0600

    update

--- MemoryGame/js/controllers/credits/creditsView.js
+++ MemoryGame/js/controllers/credits/creditsView.js
@@ -6,7 +6,7 @@ export class CreditsView extends ControllerView{
         super(controller,parent);
         this.elementsContainer.className = 'creditsView';
 
-        let text = p({appendChild:'text',innerHTML:'Add credits here'},this.elementsContainer);
+        let text = p({appendChild:'text',innerHTML:'Developed and design by Alvaro Castillo.'},this.elementsContainer);
 
 
 
