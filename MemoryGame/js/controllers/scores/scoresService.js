4a925f05bb8dac8b61787552ad7f26b2a5ca2b91
lvroc <lvroc.gnz@outlook.com>
2023-08-11 22:11:58 -0600

    update

--- MemoryGame/js/controllers/scores/scoresService.js
+++ MemoryGame/js/controllers/scores/scoresService.js
@@ -8,7 +8,8 @@ export class ScoresService extends Service {
 
     getScores() {
         let scores = [];
-        var url = `https://us-central1-cenfoprojectsbackend.cloudfunctions.net/app/scores`
+        var url = `https://flip-and-match-game.vercel.app/scores`
+       /*  var url = `https://flip-and-match-game-default-rtdb.firebaseio.com/app/scores` */
         var request = new XMLHttpRequest();
         request.open('get', url);
         request.onload = () => {
