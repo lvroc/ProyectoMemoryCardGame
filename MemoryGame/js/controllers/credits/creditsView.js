bc7b39fca45f10d5ba03187bd2b25ec060b7ad72
lvroc <lvroc.gnz@outlook.com>
2023-08-19 23:42:56 -0600

    update

--- MemoryGame/js/controllers/credits/creditsView.js
+++ MemoryGame/js/controllers/credits/creditsView.js
@@ -1,12 +1,23 @@
-import { p } from "../../libs/html.js";
+import { img, p } from "../../libs/html.js";
 import { ControllerView } from "../controllerView.js";
 
 export class CreditsView extends ControllerView{
     constructor(controller, parent){
         super(controller,parent);
         this.elementsContainer.className = 'creditsView';
+        img({
+            src: './src/img/logo.svg',
+            className: 'homeView-logo'
+        }, this.elementsContainer);
+        let text = p({appendChild:'text',innerHTML:'Developed and design by'},this.elementsContainer);
 
-        let text = p({appendChild:'text',innerHTML:'Developed and design by Alvaro Castillo.'},this.elementsContainer);
+        p({
+            innerHTML: 'LVROC StellarWebCraft',className:'p_credits_companyname'
+        }, this.elementsContainer);
+
+        p({
+            innerHTML: '2023©'
+        }, this.elementsContainer);
 
 
 
